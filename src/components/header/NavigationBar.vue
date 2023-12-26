3601fe4be62c8fe8516119e421072bc97cd82582
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-12-27 02:02:24 +0700

    Добавил компонент NavigationBar

---
+++ src/components/header/NavigationBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <ul class="navigation-list">
+        <li v-for="item, i in menuList" :key="i">
+            <a href="#"> {{ item.text }}</a>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'NavigationBar',
+    props:{
+        menuList: {
+            type: Array,
+            required: true
+        }
+    },
+    methods:{
+        hideMenu(){
+            this.$emit('changeShowMenu')
+        }
+    }
+}
+</script>
