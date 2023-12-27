e131de29588a59de04be5bbadaacaa472c32707c
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-12-27 23:13:40 +0700

    Добавил компонент Main Button

---
+++ src/components/UI/MainButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button class="main-btn" @click="action">
+        {{text}}
+    </button>
+</template>
+  
+<script>
+export default {
+    props:{
+        text:{
+            type: String,
+            required: true
+        }
+    },
+    methods:{
+        action(){
+            this.$emit('actionClick');
+        }
+    }
+}
+</script>
