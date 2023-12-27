ae1fd7c81cdeb89b5f3b6859f9b90ba6c5db3efa
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-12-27 23:18:06 +0700

    Добавил компонент E404

---
+++ src/components/E404.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="e404-wrapper">
+        <div class="container-e404">
+            <div class="e404-title">
+                404 error
+            </div>
+            <div class="e404-text">
+                This page not found;<br>
+                back to home and start again
+            </div>
+            <router-link :to="{name: 'home'}">
+                <app-main-btn :text="'homepage'"></app-main-btn>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import AppMainBtn from './UI/MainButton.vue'
+export default {
+    name: 'E404',
+    components:{
+        AppMainBtn
+   }
+}
+</script>
+
+<style>
+
+</style>
