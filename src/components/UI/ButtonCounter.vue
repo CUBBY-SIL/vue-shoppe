473c97296cb6e03d1d9664d21dab81b9786f3deb
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-12-27 23:10:49 +0700

    Добавил компонент Button Counter

---
+++ src/components/UI/ButtonCounter.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="btn-counter">
+        <button @click="decrease">-</button>
+        <div class="container-count">{{ count }}</div>
+        <button @click="increase">+</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        count:{
+            type: Number,
+            required: true
+        }
+    },
+    methods:{
+        increase(){
+            this.$emit('changeCnt', '+')
+        },
+        decrease(){
+            this.$emit('changeCnt', '-')
+        }
+    }
+}
+</script>
