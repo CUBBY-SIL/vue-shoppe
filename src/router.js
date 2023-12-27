ba04e6e096fb34c2927083d5061d8469b0cae61d
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-12-27 23:21:48 +0700

    Vue Router - добавил страницу товара

--- src/router.js
+++ src/router.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import AppHomePage from './components/pages/HomePage'
+import AppProduct from '@/components/product/Product'
 
 const routes = [
     {
         name: 'home',
         path: '/',
         component: AppHomePage
+    },
+    {
+        name: 'product',
+        path: '/product/:id',
+        component: AppProduct
     },
 ]
 
