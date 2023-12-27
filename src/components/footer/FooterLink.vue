a00b8bdb33ea5a1a42fb541ee1ca8672f54d65f4
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-12-27 20:13:07 +0700

    Добавил компонент Footer Link

---
+++ src/components/footer/FooterLink.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="footer-links__item">
+        <router-link :to="{name: `${footerLink.link}`}">{{footerLink.text}}</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FooterLink',
+    props:{
+        footerLink: {
+            required: true
+        }
+    }
+}
+</script>
