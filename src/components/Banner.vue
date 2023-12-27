36c7b7082a712eeb567039fdc0c5b1de2db8252b
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-12-27 19:02:51 +0700

    Добавил компонент Banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="banner-section">
+        <div class="banner-information">
+            <div class="banner-information__title">
+                <h1>The best jewelry</h1>
+            </div>
+            <div class="banner-information__text">
+                <p>We have the largest selection</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+name: 'Banner',
+}
+</script>
+
+<style>
+.banner-section{
+  width: 100%;
+  display: flex;
+  border-radius: 16px;
+  border-collapse: separate; 
+}
+.banner-container-content{
+    background: #171D28;
+    width: 100%;
+    flex-grow: 1;
+    flex-basis: 500px;
+}
+</style>
