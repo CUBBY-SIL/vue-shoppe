78336952d859070a95a4fefc763b670fd89a3caf
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-12-27 19:41:42 +0700

    Добавил компонент Preview Shop

---
+++ src/components/preview-shop/PreviewShop.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="preview-title">
+        <div class="preview-title__text">
+            <p>Shop The Latest</p>
+        </div>
+        <div class="preview-link">
+            <a href="#">View ALL</a>
+        </div>
+    </div>
+    <div class="preview-list">
+        <app-preview-item 
+            v-for="product in productList.slice(0,6)" 
+            :key="product.id"
+            :productImg="product.images[0]"
+            :productTitle="product.title"
+            :productPrice="product.price"
+            :productId="product.id"
+        />
+    </div>
+</template>
+
+<script>
+import AppPreviewItem from './PreviewItem.vue'
+import {mapGetters} from 'vuex';
+export default {
+  components: { AppPreviewItem },
+    name: 'PreviewShop',
+    computed:{
+        ...mapGetters('products', { productList: 'all'}),
+    }
+}
+</script>
+
+<style>
+
+</style>
