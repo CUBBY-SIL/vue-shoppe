b44c46d887292a47a58195a1ac1c0feff34d033a
CUBBY-SIL <nikita.bergen0@gmail.com>
2023-12-28 19:50:35 +0700

    Vue Router - добавил страницу каталога

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import AppHomePage from './components/pages/HomePage'
 import AppProduct from '@/components/product/Product'
+import AppCatalog from '@/components/pages/Catalog'
 
 const routes = [
     {
@@ -13,6 +14,11 @@ const routes = [
         name: 'product',
         path: '/product/:id',
         component: AppProduct
+    },
+    {
+        name: 'catalog',
+        path: '/catalog',
+        component: AppCatalog
     },
 ]
 
